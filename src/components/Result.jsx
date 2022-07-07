04f329a73b790f019c4d687db7aadff09d7a56cb
jlrn-rph <earthtostin@gmail.com>
2022-07-07 08:50:19 +0800

    change subtitle of confidence level

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,50 +1,75 @@
-import { Typography, Card, CardHeader, CardContent, Divider } from '@material-ui/core';
-import { ThemeProvider, withStyles } from '@material-ui/core/styles';
-import { theme } from './Style';
+import {
+  Typography,
+  Card,
+  CardHeader,
+  CardContent,
+  Divider,
+} from "@material-ui/core";
+import { ThemeProvider, withStyles } from "@material-ui/core/styles";
+import { theme } from "./Style";
 
-const styles = theme => ({
-    title: {
-        backgroundColor: "#00037C", 
-        fontWeight: 500, 
-        color: '#fff',
-        borderRadiusTop:'10px'
-    },
-    subtitle:{
-        color: theme.palette.secondary.dark,
-        fontWeight: 500
-    },
-    divider:{
-        marginBottom: '11px'
-    },
-    cardContent: {
-        padding: '0px 20px 0px 20px', 
-    }
+const styles = (theme) => ({
+  title: {
+    backgroundColor: "#00037C",
+    fontWeight: 500,
+    color: "#fff",
+    borderRadiusTop: "10px",
+  },
+  subtitle: {
+    color: theme.palette.secondary.dark,
+    fontWeight: 500,
+  },
+  divider: {
+    marginBottom: "11px",
+  },
+  cardContent: {
+    padding: "0px 20px 0px 20px",
+  },
 });
 
-const Result = ({ classes, index, filename, classname, classLabel, confidence }) => {
-    return(
-        <ThemeProvider theme={theme}> 
-            <CardHeader title="Result" className={classes.title} />
-                 <Card className={classes.cardContent}>
-                    <CardContent>
-                        <Typography variant="subtitle1" className={classes.subtitle}> INDEX </Typography>
-                        <Typography variant="h6">{ index }</Typography>
-                        <Divider className={classes.divider} />
+const Result = ({
+  classes,
+  index,
+  filename,
+  classname,
+  classLabel,
+  confidence,
+}) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <CardHeader title="Result" className={classes.title} />
+      <Card className={classes.cardContent}>
+        <CardContent>
+          <Typography variant="subtitle1" className={classes.subtitle}>
+            {" "}
+            INDEX{" "}
+          </Typography>
+          <Typography variant="h6">{index}</Typography>
+          <Divider className={classes.divider} />
 
-                        <Typography variant="subtitle1" className={classes.subtitle}> FILE NAME </Typography>
-                        <Typography variant="h6">{ filename }</Typography>
-                        <Divider className={classes.divider} />
+          <Typography variant="subtitle1" className={classes.subtitle}>
+            {" "}
+            FILE NAME{" "}
+          </Typography>
+          <Typography variant="h6">{filename}</Typography>
+          <Divider className={classes.divider} />
 
-                        <Typography variant="subtitle1" className={classes.subtitle}> { classLabel } </Typography>
-                        <Typography variant="h6">{ classname }</Typography>
-                        <Divider className={classes.divider} />
+          <Typography variant="subtitle1" className={classes.subtitle}>
+            {" "}
+            {classLabel}{" "}
+          </Typography>
+          <Typography variant="h6">{classname}</Typography>
+          <Divider className={classes.divider} />
 
-                        <Typography variant="subtitle1" className={classes.subtitle}> CONFIDENCE LEVEL </Typography>
-                        <Typography variant="h6">{ confidence }</Typography>
-                </CardContent>
-            </Card>
-        </ThemeProvider>
-    );
-}
+          <Typography variant="subtitle1" className={classes.subtitle}>
+            {" "}
+            CONFIDENCE LEVEL/PROBABILITY{" "}
+          </Typography>
+          <Typography variant="h6">{confidence}</Typography>
+        </CardContent>
+      </Card>
+    </ThemeProvider>
+  );
+};
 
 export default withStyles(styles)(Result);
